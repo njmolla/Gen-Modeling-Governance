fix jacobian calculation

GM_code.py
@@ -43,7 +43,7 @@ def sample(N1,N2,N3,K,M,T, C1,C2):
 
     mus = np.random.rand(1,M) # $
 
-    rhos = np.random.rand(1,M) # $
+    rhos = np.random.rand(1,M) # $ should be 1 for M=1 based on rho_bar
     rho_bars = np.reshape(1 - rhos,(1,M,1)) # want to be 1,m,1$
 
     omegas = np.zeros([1,M,M]) # omegas_m,j is 1xjxm, want to sum to 1 along m
@@ -97,13 +97,13 @@ def sample(N1,N2,N3,K,M,T, C1,C2):
     dc_dw_n = np.random.uniform(0,2,(N,N)) #dc_dw_n_i,n is ixn $
     dc_dw_n[indices,indices] = 0
     dl_dx = np.random.rand(N)
-    di_dK_p = 0#np.random.uniform(0,2,(N,M))  # $ temporary ###################
-    di_dK_n = 0#np.random.uniform(0,2,(N,M))  # $ temporary ###################
-    di_dy_p = np.zeros(1,M)# np.random.rand(1,M)  # $
-    di_dy_n = np.zeros(1,M) # np.random.rand(1,M)  # $
+    di_dK_p = np.random.uniform(0,2,(N,M))
+    di_dK_n = np.random.uniform(0,2,(N,M))
+    di_dy_p = np.random.rand(1,M)  # $
+    di_dy_n = np.random.rand(1,M)  # $
     dt_dD_jm = np.random.uniform(0,2,(N,M,M))  # dt_j->m/d(D_i,j->m * x_i) is ixmxj  $
-    dtjm_dym = np.zeros(M,M)# was np.random.rand(M,M)  # 1xmxj
-    dtmj_dym = np.zeros(M,M) #was np.random.uniform(-1,0,(1,M,M))  # 1xjxm
+    dtjm_dym = np.random.rand(M,M)  # 1xmxj
+    dtmj_dym = np.random.uniform(-1,0,(1,M,M))  # 1xjxm
 
 
     # ------------------------------------------------------------------------
@@ -169,6 +169,7 @@ def run_multiple(N1,N2,N3,K,M,T, C1,C2, num_samples):
   num_stable_webs = 0
   for _ in range(num_samples):
     stability = sample(N1,N2,N3,K,M,T,C1,C2)[0]  # stability is the first return value
+    print(stability)
     if stability:
       num_stable_webs += 1
 

compute_J.py
@@ -89,7 +89,7 @@ def determine_stability(N,K,M,T,
 
   # dy•/dy for m = j
   indices = np.arange(N+1,T)  # Access the diagonal of the governing agency part.
-  J[indices,indices] = np.squeeze(mus*rhos*di_dy_p - thetas*di_dy_n
+  J[indices,indices] = np.squeeze(mus)*np.squeeze(rhos*di_dy_p - thetas*di_dy_n
         + np.squeeze(rho_bars, axis = 2)*np.sum(omegas*dtjm_dym, axis = 2)
         - np.squeeze(theta_bars, axis = 1)*np.sum(epsilons*dtmj_dym, axis=1)
       )
@@ -99,7 +99,7 @@ def determine_stability(N,K,M,T,
   # Compute the eigenvalues of the Jacobian
   # --------------------------------------------------------------------------
   eigvals = np.linalg.eigvals(J)
-  if all(eigvals.real) < 10e-5:  # stable if real part of eigenvalues is negative
+  if all(eigvals.real < 10e-5):  # stable if real part of eigenvalues is negative
     stability = True
   else:
     stability = False  # unstable if real part is positive, inconclusive if 0

strategy_optimization.py
@@ -457,7 +457,7 @@ def grad_descent_constrained(initial_point, max_steps, n, l, J, N,K,M,T,
   grad_mag = np.linalg.norm(grad)  # to check for convergence
 
   x = initial_point  # strategy
-  alpha = 0.005 # had this 0.05
+  alpha = 0.05 # had this 0.05
   num_steps = 0
   while grad_mag > 1e-5 and num_steps < max_steps:
     # Follow the projected gradient for a fixed step size alpha
@@ -514,7 +514,7 @@ def nash_equilibrium(max_iters, J, N,K,M,T,
     strategy[i] /= np.sum(strategy[i])
   # sample to get bridging org objectives
   objectives = np.random.randint(0,N-K,size = K)
-  tolerance = 0.001 # was 0.01##################################################################################################################################################
+  tolerance = 0.01 # was 0.01##################################################################################################################################################
   strategy_difference = [1]  # arbitrary initial value, List of differences in euclidean distance between strategies in consecutive iterations
   iterations = 0
   strategy_prev = []  # a list of the strategies at each iteration
@@ -553,10 +553,11 @@ def nash_equilibrium(max_iters, J, N,K,M,T,
       print('max number of iterations')
   for i in range(len(strategy_prev)):
       diff_with_eq.append(np.linalg.norm(strategy_prev[i] - strategy_prev[-1]))
-  with open('strategy_difference_1actor.csv', 'w+') as f:
+  with open('strategies_1actor.csv', 'w+') as f:
     csvwriter = csv.writer(f)
-    csvwriter.writerow(strategy_difference)
-    csvwriter.writerow(diff_with_eq)
+    csvwriter.writerows(strategy_prev)
+#    csvwriter.writerow(strategy_difference)
+#    csvwriter.writerow(diff_with_eq)
   return F_p,F_n,H_p,H_n,W_p,W_n,K_p,K_n,D_jm, sigmas,lambdas
 
 