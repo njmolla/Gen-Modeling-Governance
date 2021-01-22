fix strategy optimization for more than one actor, calculate gradient only once each iteration

GM_code.py
@@ -140,11 +140,11 @@ def sample(N1,N2,N3,K,M,T, C1,C2):
     if is_connected == False:
       continue
 
-#    # find nash equilibrium strategies
-#    F_p,F_n,H_p,H_n,W_p,W_n,K_p,K_n,D_jm, sigmas, lambdas, converged = nash_equilibrium(2000, J, N,K,M,T,
-#        phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
-#        F_p,F_n,H_p,H_n,W_p,W_n,K_p,K_n,D_jm)
-    converged = True
+    # find nash equilibrium strategies
+    F,H,W,K_p,D_jm, sigmas, lambdas, converged = nash_equilibrium(2000, J, N,K,M,T,
+        phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
+        F,H,W,K_p,D_jm)
+    #converged = True
 
     # ------------------------------------------------------------------------
     # See if system is stable and if it is weakly connected
@@ -230,14 +230,14 @@ def run_once(N1,N2,N3,K,M,T, C1,C2):
 #      + np.size(W_p) + np.size(W_n) + np.size(K_p) + np.size(K_n) + np.size(omegas)
 #      + np.size(epsilons) + np.size(D_jm))
 
-  return (stability, J,
+  return (stability, J, converged,
           phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
           F,H,W,K_p,D_jm)
 
 
 def main():
   # Size of system
-  N1 = 1 # number of resource users that benefit from extraction only
+  N1 = 2 # number of resource users that benefit from extraction only
   N2 = 0 # number of users with both extractive and non-extractive use
   N3 = 0  # number of users with only non-extractive use
   K = 0 # number of bridging orgs
@@ -253,7 +253,7 @@ def main():
 
 
 if __name__ == "__main__":
-  (stability, J,
+  (stability, J, converged,
       phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
       F,H,W,K_p,D_jm) = main()
 

objective_gradient.py
@@ -1,9 +1,8 @@
 import numpy as np
 from compute_J import determine_stability
-import csv
 from numba import jit
 
-
+@jit(nopython=True)
 def objective_grad(strategy, n, l, J, N,K,M,T,
     phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
     F,H,W,K_p,D_jm):
@@ -36,6 +35,7 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
   # Compute inverse Jacobian
   J_inv = np.linalg.inv(J)
 
+  #TO DO: This section doesn't need to be calculated every time (except for dxdot_dws)
   # Compute how the rhs of system changes with respect to each strategy parameter
   drdot_dF = -phi*np.multiply(np.reshape(psis,(1,1,N)),np.multiply(de_dg,dg_dF))
   dxdot_dF = np.zeros([N,N,M,N])
@@ -126,8 +126,8 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
             ,axis=0)  # Sum over k
         ,axis=0)
 
-    grad_e_W = np.zeros((1,N))
-    grad_e_W[W[l]>=0] = de_dr[0,n] * dR_dW_p[l] + np.sum(np.multiply(np.reshape(de_dg[0,:,n]*dg_dy[:,n], (M,1)), dY_dW_p[:,l])
+    grad_e_W = np.zeros((N))
+    grad_e_W_p = de_dr[0,n] * dR_dW_p[l] + np.sum(np.multiply(np.reshape(de_dg[0,:,n]*dg_dy[:,n], (M,1)), dY_dW_p[:,l])
               + np.sum(
                   np.multiply(  # Both factors need to be kmi
                       np.reshape(
@@ -141,7 +141,7 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
               ,axis=0)  # Sum over k
           ,axis=0)
 
-    grad_e_W[W[l]<0] =  de_dr[0,n] * dR_dW_n[l] + np.sum(np.multiply(np.reshape(de_dg[0,:,n]*dg_dy[:,n], (M,1)), dY_dW_n[:,l])
+    grad_e_W_n =  de_dr[0,n] * dR_dW_n[l] + np.sum(np.multiply(np.reshape(de_dg[0,:,n]*dg_dy[:,n], (M,1)), dY_dW_n[:,l])
             + np.sum(
                 np.multiply(  # Both factors need to be kmji
                     np.reshape(np.multiply(de_dg[:,:,n],dg_dF[:,:,n]*F[:,:,n]), (N,M,1)),
@@ -149,9 +149,11 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
                 )
             ,axis=0)  # Sum over k
         ,axis=0)
+    grad_e_W[W[l]>=0] = grad_e_W_p[W[l]>=0]
+    grad_e_W[W[l]<0] = grad_e_W_n[W[l]<0]
 
-    grad_e_K = np.zeros((1,M))
-    grad_e_K[K_p[l]>=0] = de_dr[0,n] * dR_dK_p[l] + np.sum(np.multiply(np.reshape(de_dg[0,:,n]*dg_dy[:,n], (M,1)), dY_dK_p[:,l])
+    grad_e_K = np.zeros((M))
+    grad_e_K_p = de_dr[0,n] * dR_dK_p[l] + np.sum(np.multiply(np.reshape(de_dg[0,:,n]*dg_dy[:,n], (M,1)), dY_dK_p[:,l])
             + np.sum(
                 np.multiply(  # Both factors need to be kmji
                     np.reshape(np.multiply(de_dg[:,:,n],dg_dF[:,:,n]*F[:,:,n]), (N,M,1)),
@@ -161,7 +163,7 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
         ,axis=0)
 
 
-    grad_e_K[K_p[l]<0] = de_dr[0,n] * dR_dK_n[l] + np.sum(np.multiply(np.reshape(de_dg[0,:,n]*dg_dy[:,n], (M,1)), dY_dK_n[:,l])
+    grad_e_K_n = de_dr[0,n] * dR_dK_n[l] + np.sum(np.multiply(np.reshape(de_dg[0,:,n]*dg_dy[:,n], (M,1)), dY_dK_n[:,l])
             + np.sum(
                 np.multiply(  # Both factors need to be kmji
                     np.reshape(np.multiply(de_dg[:,:,n],dg_dF[:,:,n]*F[:,:,n]), (N,M,1)),
@@ -169,6 +171,9 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
                 )
             ,axis=0)  # Sum over k
         ,axis=0)
+
+    grad_e_K[K_p[l]>=0] = grad_e_K_p[K_p[l]>=0]
+    grad_e_K[K_p[l]<0] = grad_e_K_n[K_p[l]<0]
 
     grad_e_Djm = de_dr[0,n] * dR_dDjm[l] + np.sum(np.multiply(np.reshape(de_dg[0,:,n]*dg_dy[:,n], (M,1,1)), dY_dDjm[:,l])
             + np.sum(
@@ -200,8 +205,8 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
 
     grad_a_H[:,n] += np.multiply(da_dp[0,:,n],dp_dH[n,:,n])
 
-    grad_a_W = np.zeros((0,N))
-    grad_a_W[W[l]>=0] = da_dr[0,n]*dR_dW_p[l] + np.sum(np.multiply(np.reshape(da_dp[0,:,n]*dp_dy[:,n], (M,1)),dY_dW_p[:,l])
+    grad_a_W = np.zeros(N)
+    grad_a_W_p = da_dr[0,n]*dR_dW_p[l] + np.sum(np.multiply(np.reshape(da_dp[0,:,n]*dp_dy[:,n], (M,1)),dY_dW_p[:,l])
           + np.sum(
               np.multiply(
                 np.reshape(np.multiply(da_dp[:,:,n],dp_dH[:,:,n]*H[:,:,n]),(N,M,1)),
@@ -209,7 +214,7 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
               ),axis=0)
           ,axis=0)
 
-    grad_a_W[W[l]<0] = da_dr[0,n] * dR_dW_n[l] + np.sum(np.multiply(np.reshape(da_dp[0,:,n]*dp_dy[:,n], (M,1)),dY_dW_n[:,l])
+    grad_a_W_n = da_dr[0,n] * dR_dW_n[l] + np.sum(np.multiply(np.reshape(da_dp[0,:,n]*dp_dy[:,n], (M,1)),dY_dW_n[:,l])
           + np.sum(
               np.multiply(
                 np.reshape(np.multiply(da_dp[:,:,n],dp_dH[:,:,n]*H[:,:,n]),(N,M,1)),
@@ -217,8 +222,11 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
               ),axis=0)
           ,axis=0)
 
-    grad_a_K = np.zeros((0,M))
-    grad_a_K[K_p[l]>=0] = da_dr[0,n] * dR_dK_p[l] + np.sum(np.multiply(np.reshape(da_dp[0,:,n]*dp_dy[:,n], (M,1)),dY_dK_p[:,l])
+    grad_a_W[W[l]>=0] = grad_a_W_p[W[l]>=0]
+    grad_a_W[W[l]<0] = grad_a_W_n[W[l]<0]
+
+    grad_a_K = np.zeros(M)
+    grad_a_K_p = da_dr[0,n] * dR_dK_p[l] + np.sum(np.multiply(np.reshape(da_dp[0,:,n]*dp_dy[:,n], (M,1)),dY_dK_p[:,l])
           + np.sum(
               np.multiply(
                 np.reshape(np.multiply(da_dp[:,:,n],dp_dH[:,:,n]*H[:,:,n]),(N,M,1)),
@@ -226,13 +234,17 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
               ),axis=0)
           ,axis=0)
 
-    grad_a_K[K_p[l]<0] =  da_dr[0,n] * dR_dK_n[l] + np.sum(np.multiply(np.reshape(da_dp[0,:,n]*dp_dy[:,n], (M,1)),dY_dK_n[:,l])
+    grad_a_K_n =  da_dr[0,n] * dR_dK_n[l] + np.sum(np.multiply(np.reshape(da_dp[0,:,n]*dp_dy[:,n], (M,1)),dY_dK_n[:,l])
           + np.sum(
               np.multiply(
                 np.reshape(np.multiply(da_dp[:,:,n],dp_dH[:,:,n]*H[:,:,n]),(N,M,1)),
                 dX_dK_n[:,l:l+1,:]
               ),axis=0)
           ,axis=0)
+
+    grad_a_K[K_p[l]>=0] = grad_a_K_p[K_p[l]>=0]
+    grad_a_K[K_p[l]<0] = grad_a_K_n[K_p[l]<0]
+
 
     grad_a_Djm = da_dr[0,n] * dR_dDjm[l] + np.sum(np.multiply(np.reshape(da_dp[0,:,n]*dp_dy[:,n], (M,1,1)),dY_dDjm[:,l])
           + np.sum(
@@ -282,12 +294,12 @@ def multiply_by_inverse_jacobian(drdot_dp, dxdot_dp, dydot_dp, J_inv, T, N, M):
                  dxdot_dp,
                  dydot_dp),
              axis=0)
-  
+
   dSdot_dp = dSdot_dp.reshape(T, np.prod(shape))  # this should already be true
-  
+
   # do the actual computation
   dSS_dp = -J_inv @ dSdot_dp
-  
+
   # unpack
   dSS_dp = dSS_dp.reshape((T, *shape))
   dR_dp = dSS_dp[0]

strategy_optimization.py
@@ -2,16 +2,12 @@ import numpy as np
 from scipy import optimize
 from objective_gradient import objective_grad
 import csv
-from numba import jit
-
-
-#@jit(nopython=True)
 
 def correct_scale_params(scale_params, alloc_params, i):
   '''
   Corrects scale parameters (either sigmas or lambdas) to be consisent with optimization
   results. Takes in scale parameters (2d) and strategy parameters for a particular actor i (1d),
-  and sets scale parameters to 0 if the corresponding strategy parameters are 0, then ensures
+  and sets scale parameters to 0 if the corresponding strategy parameters are 0, ensuring
   that the scale parameters still add to 1.
   '''
   scale_params[:,i][alloc_params==0] = 0
@@ -46,13 +42,17 @@ def grad_descent_constrained(initial_point, max_steps, n, l, J, N,K,M,T,
   raw_grad = [] # for debugging
   projected_grad = [] # for debugging
   strategies = [] # for debugging
-  grad = objective_grad(initial_point, n, l, J, N,K,M,T,
-                        phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
-                        F,H,W,K_p,D_jm)
 
-  raw_grad.append(grad) # for debugging
   x = initial_point  # strategy
   strategies.append(x) # for debugging
+
+  alpha = 0.05
+
+  grad = objective_grad(x, n, l, J, N,K,M,T,
+                      phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
+                      F,H,W,K_p,D_jm)
+
+  raw_grad.append(grad) # for debugging
 
   # figure out which plane to project gradient onto
   plane = np.sign(x)
@@ -62,70 +62,87 @@ def grad_descent_constrained(initial_point, max_steps, n, l, J, N,K,M,T,
   # Project gradient onto the plane sum(efforts) == 1
   grad = grad - np.dot(grad, plane)*plane/sum(abs(plane))
   projected_grad.append(grad) # for debugging
-  grad_mag = np.linalg.norm(grad)  # to check for convergence
-
-  alpha = 0.05
-  num_steps = 0
-  while grad_mag > 1e-5 and num_steps < max_steps:
-    # Follow the projected gradient for a fixed step size alpha
-    x = x + alpha*grad
-#    print('banana:', end = ' ')
-#    print(np.sum(x*plane))
-    x /= np.sum(x*plane) # Normalize to be sure (get some errors without this)
-
-    # If strategy does not have all efforts >= 0, project onto space of legal strategies
-    if np.any(x*plane < -0.01):
-      try:
-        ub = np.sum(abs(x)) #np.sum(abs(x[x*plane>0]))
-        print()
-        print('banana')
-        print(np.sum(np.maximum(x*plane - 0, 0)) - 1)
-        print(x)
-        print(plane)
-        print(np.sum(x*plane))
-        print(np.sum(np.maximum(x*plane - ub, 0)) - 1)
-        mu = optimize.brentq(boundary_projection, 0, ub, args=(x, plane))
-      except:
-        print('bisection bounds did not work')
-        raise Exception('bisection bounds did not work')
-      x = plane * np.maximum(x*plane - mu, 0)
-    strategies.append(x)
 
 
-    """
-    print(raw_grad[-1])
-    print(projected_grad[-1])
-    print() # for debugging
-    print('raw')
-    print(raw_grad[-1]) # for debugging
-    print('projected_grad')
-    print(projected_grad[-1]) # for debugging
-    print('point')
-    print(x) # for debugging
-    print('plane')
-    print(plane) # for debugging
-    #"""
+  x = x + alpha*grad
 
-    # Compute new gradient and update strategy parameters to match x
-    grad = objective_grad(x, n, l, J, N,K,M,T,
-                          phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
-                          F,H,W,K_p,D_jm)
-    raw_grad.append(grad) # for debugging
+  x[abs(x)<0.001] = 0
+  x /= np.sum(x*plane) # Normalize to be sure (get some errors without this)
 
-    # figure out which plane to project gradient onto
-    plane = np.sign(x)
-    plane[abs(x)<0.001] = np.sign(grad[abs(x)<0.001])
-    plane[-(M**2):] = 1 # for parameters that can only be positive, set to positive
+  # If strategy does not have all efforts >= 0, project onto space of legal strategies
+  if np.any(x*plane < -0.01):
+    try:
+      ub = np.sum(abs(x)) #np.sum(abs(x[x*plane>0]))
+      mu = optimize.brentq(boundary_projection, 0, ub, args=(x, plane))
+    except:
+      print('bisection bounds did not work')
+      raise Exception('bisection bounds did not work')
+    x = plane * np.maximum(x*plane - mu, 0)
+  strategies.append(x)
 
-    # Project gradient onto the plane abs(params)=1
-    grad = grad - np.dot(grad, plane)*plane/sum(abs(plane))
-    projected_grad.append(grad) # for debugging
-
-    grad_mag = np.linalg.norm(grad)  # to check for convergence
-
-    num_steps += 1
-    if grad_mag < 1e-5:
-      print('gradient descent convergence reached')
+#  num_steps = 0
+#
+#  while grad_mag > 1e-5 and num_steps < max_steps:
+#    # Follow the projected gradient for a fixed step size alpha
+#    x = x + alpha*grad
+##    print('banana:', end = ' ')
+##    print(np.sum(x*plane))
+#    x[abs(x)<0.001] = 0
+#    x /= np.sum(x*plane) # Normalize to be sure (get some errors without this)
+#
+#    # If strategy does not have all efforts >= 0, project onto space of legal strategies
+#    if np.any(x*plane < -0.01):
+#      try:
+#        ub = np.sum(abs(x)) #np.sum(abs(x[x*plane>0]))
+##        print()
+##        print('banana')
+##        print(np.sum(np.maximum(x*plane - 0, 0)) - 1)
+##        print(x)
+##        print(plane)
+##        print(np.sum(x*plane))
+##        print(np.sum(np.maximum(x*plane - ub, 0)) - 1)
+#        mu = optimize.brentq(boundary_projection, 0, ub, args=(x, plane))
+#      except:
+#        print('bisection bounds did not work')
+#        raise Exception('bisection bounds did not work')
+#      x = plane * np.maximum(x*plane - mu, 0)
+#    strategies.append(x)
+#
+#
+#    """
+#    print(raw_grad[-1])
+#    print(projected_grad[-1])
+#    print() # for debugging
+#    print('raw')
+#    print(raw_grad[-1]) # for debugging
+#    print('projected_grad')
+#    print(projected_grad[-1]) # for debugging
+#    print('point')
+#    print(x) # for debugging
+#    print('plane')
+#    print(plane) # for debugging
+#    #"""
+#
+#    # Compute new gradient and update strategy parameters to match x
+#    grad = objective_grad(x, n, l, J, N,K,M,T,
+#                          phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
+#                          F,H,W,K_p,D_jm)
+#    raw_grad.append(grad) # for debugging
+#
+#    # figure out which plane to project gradient onto
+#    plane = np.sign(x)
+#    plane[abs(x)<0.001] = np.sign(grad[abs(x)<0.001])
+#    plane[-(M**2):] = 1 # for parameters that can only be positive, set to positive
+#
+#    # Project gradient onto the plane abs(params)=1
+#    grad = grad - np.dot(grad, plane)*plane/sum(abs(plane))
+#    projected_grad.append(grad) # for debugging
+#
+#    grad_mag = np.linalg.norm(grad)  # to check for convergence
+#
+#    num_steps += 1
+#    if grad_mag < 1e-5:
+#      print('gradient descent convergence reached')
   return x, raw_grad, projected_grad, strategies # normally return only x
 
 
@@ -168,7 +185,7 @@ def nash_equilibrium(max_iters,J,N,K,M,T,
         objective = objectives[i-(N-K)]
 
 
-      new_strategy = grad_descent_constrained(strategy[i], 1, objective, i, J, N,K,M,T,
+      new_strategy, raw_grad, projected_grad, strategies = grad_descent_constrained(strategy[i], 1, objective, i, J, N,K,M,T,
           phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
           F,H,W,K_p,D_jm)
       # Check if there are new zeros in the strategy parameters to see if we need to update scale parameters
@@ -190,9 +207,9 @@ def nash_equilibrium(max_iters,J,N,K,M,T,
       converged = False
   for i in range(len(strategy_prev)):
       diff_with_eq.append(np.linalg.norm(strategy_prev[i] - strategy_prev[-1]))
-  with open('strategies_1actor.csv', 'w+') as f:
-    csvwriter = csv.writer(f)
-    csvwriter.writerows(np.array(strategy_prev))
+#  with open('strategies_1actor.csv', 'w+') as f:
+#    csvwriter = csv.writer(f)
+#    csvwriter.writerows(np.array(strategy_prev))
 #    csvwriter.writerow(strategy_difference)
 #    csvwriter.writerow(diff_with_eq)
   return F,H,W,K_p,D_jm, sigmas,lambdas, converged