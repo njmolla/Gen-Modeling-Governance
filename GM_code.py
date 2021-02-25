small fix to gradient calculation, some reorganizing of optimization

GM_code.py
@@ -105,7 +105,7 @@ def sample(N1,N2,N3,K,M,T, C1,C2):
     dc_dw_p[indices,indices] = 0
     dc_dw_n = np.random.uniform(0,2,(N,N)) #dc_dw_n_i,n is ixn $
     dc_dw_n[indices,indices] = 0
-    dl_dx = np.random.uniform(0,1,(N)) #only converges if this is >=0.8
+    dl_dx = np.random.uniform(0,1,(N)) # more likely to converge if this is >=0.8
     di_dK_p = np.random.uniform(0,2,(N,M))#np.zeros((N,M))#np.random.uniform(0,2,(N,M))
     di_dK_n = np.random.uniform(0,2,(N,M))#np.zeros((N,M))#np.random.uniform(0,2,(N,M))
     di_dy_p = np.random.rand(1,M)  # $
@@ -147,9 +147,15 @@ def sample(N1,N2,N3,K,M,T, C1,C2):
       continue
 
     # find nash equilibrium strategies
-    F,H,W,K_p,D_jm, sigmas, lambdas, converged, strategy_history = nash_equilibrium(500, J, N,K,M,T,
-        phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym)
-
+#    F,H,W,K_p,D_jm, sigmas, lambdas, converged, strategy_history, grad = nash_equilibrium(500, J, N,K,M,T,
+#        phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym)
+#    # check whether result makes sense
+    if np.all(np.diag(np.fliplr(np.squeeze(F)))*np.squeeze(de_dg)[::-1] <= 1e-5):
+      logical = True
+    else:
+      logical = False
+    #print(logical)
+    #print(-np.diag(np.fliplr(np.squeeze(F)))*np.squeeze(de_dg)[::-1])
     # ------------------------------------------------------------------------
     # See if system is stable and if it is weakly connected
     # ------------------------------------------------------------------------
@@ -166,7 +172,7 @@ def sample(N1,N2,N3,K,M,T, C1,C2):
     if is_connected == False:
       print('not weakly connected')
 
-  return (stability, J, converged, strategy_history,
+  return (stability, J, converged, strategy_history, logical, grad,
       phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
       F,H,W,K_p,D_jm)
 
@@ -179,50 +185,58 @@ def run_multiple(size,C1,C2,num_samples):
   num_stable_webs = 0
   num_stable_webs_filtered = 0
   num_converged = 0
-
-  for _ in range(num_samples):
+  num_logical = 0
+  np.random.seed(0)
+  for i in range(num_samples):
+    print('figure ' + str(i+1))
       # Need at least 2 resource users and one gov org
-    N = 1
+    N1 = 2
+    N2 = 0
+    N3 = 0
+    K = 0
     M = 1
-    rand = np.random.rand(size-3)
-    N += np.sum(rand < 0.6)
-    K = np.sum(rand < 0.8) - np.sum(rand < 0.6)
-    M += np.sum(rand > 0.8)
-    rand2 = np.random.rand(N-1)
-    # choose at random whether guaranteed extractor is just extractor or extractor + accessor
-    rand3 = np.random.rand(1)
-    if rand3 < 0.5:
-      N1 = 1 + np.sum(rand2 < 0.33)
-      N2 = np.sum(rand2 > 0.33) - np.sum(rand2 > 0.66)
-    else:
-      N1 = np.sum(rand2 < 0.33)
-      N2 = 1 + np.sum(rand2 > 0.33) - np.sum(rand2 > 0.66)
-    N3 = np.sum(rand2 > 0.66)
+#    rand = np.random.rand(size-3)
+#    N += np.sum(rand < 0.6)
+#    K = np.sum(rand < 0.8) - np.sum(rand < 0.6)
+#    M += np.sum(rand > 0.8)
+#    rand2 = np.random.rand(N-1)
+#    # choose at random whether guaranteed extractor is just extractor or extractor + accessor
+#    rand3 = np.random.rand(1)
+#    if rand3 < 0.5:
+#      N1 = 1 + np.sum(rand2 < 0.33)
+#      N2 = np.sum(rand2 > 0.33) - np.sum(rand2 > 0.66)
+#    else:
+#      N1 = np.sum(rand2 < 0.33)
+#      N2 = 1 + np.sum(rand2 > 0.33) - np.sum(rand2 > 0.66)
+#    N3 = np.sum(rand2 > 0.66)
     N = N1 + N2 + N3 + K # total number of resource users
     T = N + M + 1 # total number of state variables
-    print((N1,N2,N3,K,M))
+#    print((N1,N2,N3,K,M))
     try:
       result = sample(N1,N2,N3,K,M,T,C1,C2)
     except:
       continue
     stability = result[0] # stability is the first return value
     converged = result[2]
+    logical = result[4]
     if stability:
       num_stable_webs += 1
       if converged:
         num_stable_webs_filtered += 1
     if converged:
       num_converged += 1
+    if logical:
+      num_logical += 1
 
-  return num_stable_webs, num_stable_webs_filtered, num_converged  # proportion of stable webs
+  return num_stable_webs, num_stable_webs_filtered, num_converged, num_logical  # proportion of stable webs
 
 
 def run_once(N1,N2,N3,K,M,T, C1,C2):
   '''
   Do a single run and return more detailed output.
   '''
-  np.random.seed(2)
-  (stability, J, converged, strategy_history,
+  np.random.seed(0)
+  (stability, J, converged, strategy_history, logical, grad,
       phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
       F,H,W,K_p,D_jm) = sample(N1,N2,N3,K,M,T,C1,C2)
 
@@ -234,7 +248,7 @@ def run_once(N1,N2,N3,K,M,T, C1,C2):
 #      + np.size(W_p) + np.size(W_n) + np.size(K_p) + np.size(K_n) + np.size(omegas)
 #      + np.size(epsilons) + np.size(D_jm))
 
-  return (stability, J, converged, strategy_history,
+  return (stability, J, converged, strategy_history, logical, grad,
           phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
           F,H,W,K_p,D_jm)
 
@@ -257,7 +271,7 @@ def main():
 
 
 if __name__ == "__main__":
-  (stability, J, converged, strategy_history,
+  (stability, J, converged, strategy_history, logical, grad,
       phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
       F,H,W,K_p,D_jm) = main()
 

compute_J.py
@@ -32,9 +32,9 @@ def determine_stability(N,K,M,T,
                                            # 1xn
   # dx•/dx for n != i
   W_p = np.zeros((N,N))
-  W_p[W>0.001] = W[W>0.001]
+  assign_when(W_p, W, W>=0)
   W_n = np.zeros((N,N))
-  W_n[W<-0.001] = W[W<-0.001]
+  assign_when(W_n, W, W<0)
   J[1:N+1,1:N+1] = np.transpose(np.multiply(alphas,
         np.multiply(betas*db_de,       np.sum(np.multiply(de_dg, dg_dF*F), axis = 1))
                      #  1xn                                ixmxn
@@ -62,9 +62,9 @@ def determine_stability(N,K,M,T,
   # dy•/dr = 0
   # dy•/dx, result is mxi
   K_plus = np.zeros((N,M))
-  K_plus[K_p>0] = K_p[K_p>0]
+  assign_when(K_plus, K_p, K_p>=0)
   K_n = np.zeros((N,M))
-  K_n[K_p<0] = abs(K_p[K_p<0])
+  assign_when(K_n, abs(K_p), K_p>=0)
   J[N+1:,1:N+1] = np.transpose(np.multiply(mus,
         np.multiply(rhos,di_dK_p*K_plus)
         - np.multiply(thetas,di_dK_n*K_n)
@@ -101,3 +101,20 @@ def determine_stability(N,K,M,T,
     stability = False  # unstable if real part is positive, inconclusive if 0
 
   return J, eigvals, stability
+
+"""
+This does
+   lhs[conditions] = rhs[conditions]
+lhs and rhs are any numpy arrays with the same shape
+conditions is a boolean numpy array with the same shape
+
+For example, to do
+   x[x > 0] = y[x > 0]
+use
+    assign_when(x, y, x > 0)
+"""
+@jit(nopython=True)
+def assign_when(lhs, rhs, conditions):
+  for nd_index in np.ndindex(lhs.shape):
+    if conditions[nd_index]:
+      lhs[nd_index] = rhs[nd_index]

objective_gradient.py
@@ -1,8 +1,9 @@
 import numpy as np
 from compute_J import determine_stability
+from compute_J import assign_when
 from numba import jit
 
-@jit(nopython=True)
+#@jit(nopython=True)
 def objective_grad(strategy, n, l, J, N,K,M,T,
     phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
     F,H,W,K_p,D_jm):
@@ -31,7 +32,7 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
   J, eigvals, stability = determine_stability(N,K,M,T,
       phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
       F,H,W,K_p,D_jm)
-
+  #print('Jacobian:',str(J))
   # Compute inverse Jacobian
   J_inv = np.linalg.inv(J)
 
@@ -46,6 +47,7 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
         np.multiply(de_dg,dg_dF)
                   # 1mn    kmn
       ), (2,0,1))  # transpose kmn -> nkm
+
   dydot_dF = np.zeros([M,N,M,N])
 
   drdot_dH = np.zeros([N,M,N])
@@ -86,6 +88,8 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
 
   ## Compute how the steady state of the system changes with respect to each strategy parameter
   dR_dF, dX_dF, dY_dF = multiply_by_inverse_jacobian(drdot_dF, dxdot_dF, dydot_dF, J_inv, T,N,M)
+  print('')
+  print('dR_dF:', str(dR_dF))
   dR_dH, dX_dH, dY_dH = multiply_by_inverse_jacobian(drdot_dH, dxdot_dH, dydot_dH, J_inv, T,N,M)
   dR_dW_p, dX_dW_p, dY_dW_p = multiply_by_inverse_jacobian(drdot_dW_p, dxdot_dW_p, dydot_dW_p, J_inv, T,N,M)
   dR_dW_n, dX_dW_n, dY_dW_n = multiply_by_inverse_jacobian(drdot_dW_n, dxdot_dW_n, dydot_dW_n, J_inv, T,N,M)
@@ -149,8 +153,8 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
                 )
             ,axis=0)  # Sum over k
         ,axis=0)
-    grad_e_W[W[l]>=0] = grad_e_W_p[W[l]>=0]
-    grad_e_W[W[l]<0] = grad_e_W_n[W[l]<0]
+    assign_when(grad_e_W, grad_e_W_p, W[l]>=0)
+    assign_when(grad_e_W, grad_e_W_n, W[l]<0)
 
     grad_e_K = np.zeros((M))
     grad_e_K_p = de_dr[0,n] * dR_dK_p[l] + np.sum(np.multiply(np.reshape(de_dg[0,:,n]*dg_dy[:,n], (M,1)), dY_dK_p[:,l])
@@ -172,8 +176,8 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
             ,axis=0)  # Sum over k
         ,axis=0)
 
-    grad_e_K[K_p[l]>=0] = grad_e_K_p[K_p[l]>=0]
-    grad_e_K[K_p[l]<0] = grad_e_K_n[K_p[l]<0]
+    assign_when(grad_e_K, grad_e_K_p, K_p[l]>=0)
+    assign_when(grad_e_K, grad_e_K_n, K_p[l]<0)
 
     grad_e_Djm = de_dr[0,n] * dR_dDjm[l] + np.sum(np.multiply(np.reshape(de_dg[0,:,n]*dg_dy[:,n], (M,1,1)), dY_dDjm[:,l])
             + np.sum(
@@ -221,9 +225,8 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
                 dX_dW_n[:,l:l+1,:]
               ),axis=0)
           ,axis=0)
-
-    grad_a_W[W[l]>=0] = grad_a_W_p[W[l]>=0]
-    grad_a_W[W[l]<0] = grad_a_W_n[W[l]<0]
+    assign_when(grad_a_W, grad_a_W_p, W[l]>=0)
+    assign_when(grad_a_W, grad_a_W_n, W[l]<0)
 
     grad_a_K = np.zeros(M)
     grad_a_K_p = da_dr[0,n] * dR_dK_p[l] + np.sum(np.multiply(np.reshape(da_dp[0,:,n]*dp_dy[:,n], (M,1)),dY_dK_p[:,l])
@@ -241,10 +244,8 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
                 dX_dK_n[:,l:l+1,:]
               ),axis=0)
           ,axis=0)
-
-    grad_a_K[K_p[l]>=0] = grad_a_K_p[K_p[l]>=0]
-    grad_a_K[K_p[l]<0] = grad_a_K_n[K_p[l]<0]
-
+    assign_when(grad_a_K, grad_a_K_p, K_p[l]>=0)
+    assign_when(grad_a_K, grad_a_K_n, K_p[l]<0)
 
     grad_a_Djm = da_dr[0,n] * dR_dDjm[l] + np.sum(np.multiply(np.reshape(da_dp[0,:,n]*dp_dy[:,n], (M,1,1)),dY_dDjm[:,l])
           + np.sum(
@@ -253,7 +254,6 @@ def objective_grad(strategy, n, l, J, N,K,M,T,
                 dX_dDjm[:,l:l+1,:,:]
               ),axis=0)
           ,axis=0)
-
 
 #  print(np.concatenate((grad_e_F.flatten(),
 #                           grad_e_H.flatten(),
@@ -316,3 +316,4 @@ def multiply_by_inverse_jacobian(drdot_dp, dxdot_dp, dydot_dp, J_inv, T, N, M):
   dX_dW_n = dSS_dW_n.reshape(T,N,N)[1:N+1]
   dY_dW_n = dSS_dW_n.reshape(T,N,N)[N+1:N+1+M]
 """
+

strategy_optimization.py
@@ -23,7 +23,7 @@ def boundary_projection(mu, strategy, plane):
   return np.sum(np.maximum(strategy*plane - mu, 0)) - 1
 
 
-def grad_descent_constrained(initial_point, n, l, J, N,K,M,T,
+def grad_descent_constrained(initial_point, alpha, n, l, J, N,K,M,T,
     phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
     F,H,W,K_p,D_jm):
   '''
@@ -41,8 +41,6 @@ def grad_descent_constrained(initial_point, n, l, J, N,K,M,T,
   '''
 
   x = initial_point  # strategy
-
-  alpha = 0.005
 
   grad = objective_grad(x, n, l, J, N,K,M,T,
     phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
@@ -93,23 +91,28 @@ def nash_equilibrium(max_iters,J,N,K,M,T,
   W = np.zeros((N,N))  # effort for collaboration. W_i,n is ixn $
   K_p = np.zeros((N,M))  # effort for more influence for gov orgs $
   D_jm = np.zeros((N,M,M))  # D_i,j,m is ixmxj effort for transferring power from each gov org to each other $
-
+  # step size
+  alpha = 0.0005
 
   # Initialize strategy
-  strategy = np.random.rand(N, 2*M*N + N + M + M**2)
-  strategy /= np.sum(np.squeeze(strategy),axis=0)
+  strategy = np.random.uniform(-1,1,size = (N, 2*M*N + N + M + M**2))
+  strategy /= np.sum(abs(np.squeeze(strategy)),axis=0)
   # sample to get bridging org objectives
   objectives = np.random.randint(0,N-K,size = K)
-  tolerance = 0.005 #
-  strategy_difference = [1]  # arbitrary initial value, List of differences in euclidean distance between strategies in consecutive iterations
+  tolerance = 10*alpha #
+  max_diff = 1  # arbitrary initial value, List of differences in euclidean distance between strategies in consecutive iterations
   iterations = 0
+  strategy_diffs = []
   strategy_history = []  # a list of the strategies at each iteration
   strategy_history.append(strategy.copy())
   converged = True
   grad = np.zeros(np.shape(strategy))
   grad_history = []
   #
-  while strategy_difference[-1] > tolerance and iterations < max_iters:
+  while max_diff > tolerance and iterations < max_iters:
+    if iterations > 300:
+      alpha = 0.0001
+
     # Loop through each actor i
     for i in range(N):
       if i <= N-K-1:
@@ -117,7 +120,7 @@ def nash_equilibrium(max_iters,J,N,K,M,T,
       else:
         objective = objectives[i-(N-K)]
 
-      new_strategy, raw_grad = grad_descent_constrained(strategy[i], objective, i, J, N,K,M,T,
+      new_strategy, raw_grad = grad_descent_constrained(strategy[i], alpha, objective, i, J, N,K,M,T,
           phi,psis,alphas,betas,beta_hats,beta_tildes,sigmas,etas,lambdas,eta_bars,mus,rhos,rho_bars,thetas,theta_bars,omegas,epsilons,ds_dr,de_dr,de_dg,dg_dF,dg_dy,dp_dy,db_de,da_dr,dq_da,da_dp,dp_dH,dc_dw_p,dc_dw_n,dl_dx,di_dK_p,di_dK_n,dt_dD_jm,di_dy_p,di_dy_n,dtjm_dym,dtmj_dym,
           F,H,W,K_p,D_jm)
 
@@ -135,13 +138,23 @@ def nash_equilibrium(max_iters,J,N,K,M,T,
     # update strategies for all actors
     strategy_history.append(strategy.copy())
     grad_history.append(grad.copy())
-    # compute difference in strategies
-    strategy_difference.append(np.linalg.norm((strategy_history[-2] - strategy_history[-1])))
+    if iterations >= 30:
+      # compute difference in strategies
+      strategy_history_10 = np.array(strategy_history[-30:]).reshape((30,N*(2*M*N + N + M + M**2)))
+      strategy_diff = np.linalg.norm(strategy_history_10[:29,:]-strategy_history_10[-1,:], axis = 1)
+      strategy_diffs.append(strategy_diff[-1])
+      max_diff = max(strategy_diff)
     iterations += 1
     if iterations == max_iters - 1:
       converged = False
-    plt.plot(np.array(strategy_difference))
-  print(strategy)
-  return F,H,W,K_p,D_jm, sigmas,lambdas, converged, strategy_history
+  plt.figure()
+  strategy_diffs = [0]*31 + strategy_diffs
+  plt.plot(np.array(strategy_diffs)/max(strategy_diffs))
+  strategy_history = np.array(strategy_history).reshape(len(strategy_history),N*(2*M*N + N + M + M**2))
+  grad_history = np.array(grad_history).reshape(len(grad_history),N*(2*M*N + N + M + M**2))
+  dist_from_conv = np.linalg.norm(strategy_history[-1] - strategy_history, axis = 1)
+  plt.plot(dist_from_conv/max(dist_from_conv),'.')
+  plt.plot(strategy_history[:,1])
+  return F,H,W,K_p,D_jm, sigmas,lambdas, converged, strategy_history, grad_history
 
 